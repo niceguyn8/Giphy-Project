7d54f93e123a07609672687a14c4b1359c6daccc
Nathan Schubert <master.control@Nathans-MacBook-Air.local>
2018-03-23 15:00:23 -0400

    gifs animate after click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -48,14 +48,33 @@ $("body").on("click", 'button', function() {
       var rating = results[i].rating;
       var p = $("<p>").text("Rating: " + rating);
       var topicImage = $("<img>");
-      topicImage.attr("src", results[i].images.fixed_height.url);
+      topicImage.attr("src", results[i].images.fixed_height_still.url);
+      // add data attribute to make gif image still
+      topicImage.attr("data-still",results[i].images.fixed_height_still.url);
+      // add data attribute to animate gif image
+      topicImage.attr("data-animate",results[i].images.fixed_height.url);
+      topicImage.attr("data-state", "still");
 
       gifDiv.prepend(p);
       gifDiv.prepend(topicImage);
 
       $("#gif-view").prepend(gifDiv);
-      // buttonGenerator();
       }
     });
     });
   });
+
+  // pause gifs when gif is clicked
+  // $('#document').on('click', 'img', function() {
+  $(document).on('click', 'img', function() {
+    var state = $(this).attr('data-state');
+
+    if (state == 'still') {
+      $(this).attr('src', $(this).attr('data-animate'));
+      $(this).attr('data-state', 'animate');
+    } else {
+      $(this).attr('src', $(this).attr('data-still'));
+      $(this).attr('data-state', 'still');
+    }
+
+  })
